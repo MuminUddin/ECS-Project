62192e8b6ec862c1508899a219cd782aa99e7fa0
MuminUddin <mumin.uddin@hotmail.com>
2026-01-08 07:45:31 +0000

    Implemented force_delete to ecr resource for images

--- infra/modules/ecr/main.tf
+++ infra/modules/ecr/main.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "gatus_ecr" {
   name = var.repo_name
   image_tag_mutability = "MUTABLE"
+  force_delete = true
 
   tags = merge(var.common_tags, {
     Name = "gatus-ecr"
